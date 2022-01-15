571330d916ca3b6e85c858fc5cad201c760fbe51
Verthezest <gmkipladen@gmail.com>
2022-01-15 17:23:06 +0700

    fix(*): ui + ci

--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -5,63 +5,68 @@ import { getPostBySlug, getAllPosts } from 'lib/postApi'
 import { markdownToHtml } from 'lib/markdownToHtml'
 
 export default function Post({ post, morePosts, preview }) {
-  const router = useRouter()
-  if (!router.isFallback && !post?.slug) {
-    return <ErrorPage statusCode={404} />
-  }
-  return (
-    <div>
-      {router.isFallback ? (
-        `Loading…`
-      ) : (
-        <>
-          <article className="mb-32">
-            <Head>
-              <title>{post.title} | Next.js Blog Example</title>
-              <meta property="og:image" content={post.ogImage.url} />
-            </Head>
-            <h1>{post.title}</h1>
-            <span>{post.content}</span>
-          </article>
-        </>
-      )}
-    </div>
-  )
+    const router = useRouter()
+    if (!router.isFallback && !post?.slug) {
+        return <ErrorPage statusCode={404} />
+    }
+    return (
+        <div>
+            {router.isFallback ? (
+                `Loading…`
+            ) : (
+                <>
+                    <article className="mb-32">
+                        <Head>
+                            <title>{post.title} | Next.js Blog Example</title>
+                            <meta
+                                property="og:image"
+                                content={post.ogImage.url}
+                            />
+                        </Head>
+                        <h1>{post.title}</h1>
+                        <span
+                            dangerouslySetInnerHTML={{ __html: post.content }}
+                        />
+                    </article>
+                </>
+            )}
+        </div>
+    )
 }
 
 export async function getStaticProps({ params }) {
-  const post = getPostBySlug(params.slug, [
-    'title',
-    'date',
-    'slug',
-    'author',
-    'content',
-    'ogImage',
-    'coverImage',
-  ])
-  const content = await markdownToHtml(post.content || '')
+    const post = getPostBySlug(params.slug, [
+        'title',
+        'date',
+        'slug',
+        'author',
+        'content',
+        'ogImage',
+        'coverImage',
+    ])
+    const content = await markdownToHtml(post.content || '')
 
-  return {
-    props: {
-      post: {
-        ...post,
-        content,
-      },
-    },
-  }
+    return {
+        props: {
+            post: {
+                ...post,
+                content,
+            },
+        },
+    }
 }
 
 export async function getStaticPaths() {
-  const posts = getAllPosts(['slug'])
+    const posts = getAllPosts(['slug'])
 
-  return {
-    paths: posts.map((post) => {
-      return {
-        params: {
-          slug: post.slug,
-        },
-      }
-    }),
-    fallback: false,
-  }
+    return {
+        paths: posts.map((post) => {
+            return {
+                params: {
+                    slug: post.slug,
+                },
+            }
+        }),
+        fallback: false,
+    }
 }
